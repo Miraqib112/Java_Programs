53df5ab6d1623709604a9512af137ff9978469e6
Mohamad Aaqib Mir <91409535+Miraqib112@users.noreply.github.com>
2022-09-13 21:02:28 +0530

    Add files via upload

---
+++ Import.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+import mypackage.Demo;
+public class Import {
+    public static void main(String[] args) {
+        Demo d1=new Demo();
+        System.out.print("I am imported from ");
+        d1.display();
+    }
+}
